4643ba1f67187c5cecb6e81fbc0f4c9e85f422d6
Pristql <luigimansion111@gmail.com>
2026-05-18 20:23:38 +0530

    Add files via upload

---
+++ L7A1.py
@@ -0,0 +1,12 @@
+x="hi"
+if (type(x) is str):
+    print("x is a string")
+else:
+    print("x is not a string")
+
+y=20
+z=20
+if y is z:
+    print("y and z are equal")
+else:
+    print("y and z are not equal")
